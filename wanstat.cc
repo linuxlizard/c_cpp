35d923e41722a9bcc31dacec6b7c8c9b86b7c635
David Poole <dpoole@cradlepoint.com>
2018-12-28 11:14:09 -0700

    it works!!!

--- wanstat.cc
+++ wanstat.cc
@@ -8,7 +8,8 @@
  * dnf install cpprest-devel boost-devel
  * sudo apt-get install libcpprest-dev  # seems to drag along the rest of the dependencies
  *
- * davep 20180705 ;
+ * davep 20180705 ; Tired of Python's problems. Let's try my getting CP router WAN status with C++
+ *
  */
 #include <cpprest/http_client.h>
 #include <cpprest/filestream.h>
@@ -16,8 +17,9 @@
 #include <chrono>
 #include <boost/program_options.hpp>
 #include <cstdlib>
-//#include <boost/regex.hpp>
 #include <regex>
+#include <boost/format.hpp>
+#include <boost/algorithm/string/join.hpp>
 
 using namespace utility;                    // Common utilities like string conversions
 using namespace web;                        // Common features like URIs.
@@ -25,89 +27,142 @@ using namespace web::http;                  // Common HTTP functionality
 using namespace web::http::client;          // HTTP client features
 using namespace concurrency::streams;       // Asynchronous streams
 
-namespace po = boost::program_options;
+// all the examples/tutorials use 'po = boost::program_options' but 'po' makes
+// me think of gettext language files
+namespace opt = boost::program_options;
 
-void parse_uri(const std::string& uri)
+struct args {
+	std::string target;
+	int debug;
+};
+
+bool parse_uri(const std::string& uri)
 {
 	/* davep 20180818 ; playing with RFC3986 */
-	std::cmatch what;
+	std::smatch what;
+//	std::cmatch what;
 
 	// https://tools.ietf.org/html/rfc3986#page-50
-	std::regex uri_regex("^[^:/?#]+://",
+	std::regex uri_regex("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?",
 			std::regex::extended);
 //	std::regex uri_regex("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?",
 //			std::regex::extended);
 
-	// interesting that regex_match() doesn't take std::string but wants a c_string
-	if (std::regex_match(uri.c_str(), what, uri_regex)) {
-		std::cout << "Match!\n";
+	if ( !std::regex_match(uri, what, uri_regex)) {
+		std::cerr << "no match\n";
+		return false;
 	}
 
+	std::string match_s = what.str();
+	std::cout << "Match! " << match_s << "\n";
+
+	std::cout << "Found " << what.size() << " matches\n";
+
+	for (auto iter=what.begin() ; iter != what.end() ; iter++ ) {
+		std::cout << *iter << "\n";
+	}
+	return true;
+}
+
+bool parse_args(int argc, char *argv[], struct args& args)
+{
+	// https://stackoverflow.com/questions/tagged/boost-program-options
+
+	// https://www.boost.org/doc/libs/1_67_0/doc/html/program_options/tutorial.html#id-1.3.31.4.3
+	// https://theboostcpplibraries.com/boost.program_options
+	opt::options_description desc{"Allowed options"};
+	desc.add_options()
+		("help,h", "produce help message")
+		// https://stackoverflow.com/questions/31921200/how-to-have-an-optional-option-value-in-boost-program-options
+		("debug,d", opt::value<int>(&args.debug)->default_value(0)->implicit_value(1), "set debug level")
+		("target,t", opt::value<std::string>()->required(), "target url e.g. http://192.168.0.1")
+	;
+	opt::variables_map argmap;
+	try {
+		opt::store(opt::parse_command_line(argc, argv, desc), argmap);
+	}
+	catch (const opt::error &err) {
+		std::cerr << err.what() << "\n";
+		return false;
+	}
+
+	// https://stackoverflow.com/questions/5395503/required-and-optional-arguments-using-boost-library-program-options
+	if (argmap.count("help")) {
+		std::cout << desc << std::endl;
+		return false;
+	}
+
+	try {
+		opt::notify(argmap);    
+	}
+	catch (const opt::error &err) {
+		std::cerr << err.what() << "\n";
+		return false;
+	}
+
+	args.target = argmap["target"].as<std::string>();
+
+	/* final step is verifying the validity of the URL */
+	return parse_uri(args.target);
 }
 
 int main(int argc, char* argv[])
 {
-	// https://www.boost.org/doc/libs/1_67_0/doc/html/program_options/tutorial.html#id-1.3.31.4.3
-	po::options_description desc("Allowed options");
-	desc.add_options()
-		("help", "produce help message")
-		("debug", po::value<int>(), "set debug level")
-		("target", po::value<std::string>(), "target")
-	;
-	po::variables_map vm;        
-	po::store(po::parse_command_line(argc, argv, desc), vm);
-	po::notify(vm);    
+	struct args args;
 
-	if (vm.count("help")) {
-		std::cout << desc << std::endl;
-		return 1;
+	if (!parse_args(argc, argv, args) ) {
+		return EXIT_FAILURE;
 	}
 
-	parse_uri(vm["target"].as<std::string>());
-	return 0;
+	std::cout << "target=" << args.target << "\n";
+	std::cout << "debug=" << args.debug << "\n";
 
 	// some defaults
-	std::string scheme = U("http");
+	std::string scheme = U("https");
 	std::string username = U("admin");
 	std::string password = U("12345");
-	int http_port = 80;
+	int http_port = 443;
 	std::string host;
 
+	if (!web::uri::validate(args.target)) {
+		std::cerr << "invalid target\n";
+		return EXIT_FAILURE;
+	}
 
-	if (vm.count("target")) {
-		std::string target_str = vm["target"].as< std::string >();
-		std::cout << "target=" << target_str << "\n";
-		if (!web::uri::validate(target_str)) {
-			std::cerr << "invalid target\n";
-			return EXIT_FAILURE;
-		}
+	web::uri target_uri = web::uri(args.target);
 
-		auto target_uri = web::uri(target_str);
-		auto target_fields = target_uri.split_query(target_str);
-		auto target_vec = target_uri.split_path(target_str);
+	//	std::map<utility::string_t, utility::string_t> target_fields = target_uri.split_query(args.target);
+	auto target_fields = target_uri.split_query(args.target);
+	for (auto iter=target_fields.begin() ; iter != target_fields.end() ; iter++ ) {
+		std::cout << "target_fields " << (*iter).first << "\n";
+	}
 
-		scheme = target_uri.scheme();
-		std::cout << "scheme=" << scheme << "\n";
-		host = target_uri.host();
-		std::cout << "host=" << target_uri.host() << "\n";
-		std::cout << "user_info=" << target_uri.user_info() << "\n";
-		std::cout << "uri=" << target_uri.to_string() << "\n";
-		http_port = target_uri.port();
-		std::cout << "http_port=" << http_port << "\n";
+	// std::vector<utility::string_t>
+	auto target_vec = target_uri.split_path(args.target);
+	for (auto iter=target_vec.begin() ; iter != target_vec.end() ; iter++ ) {
+		std::cout << "target_vec " << *iter << "\n";
+	}
 
-		std::cout << "is_authority=" << target_uri.is_authority() << "\n";
+	scheme = target_uri.scheme();
+	std::cout << "scheme=" << scheme << "\n";
+	host = target_uri.host();
+	std::cout << "host=" << target_uri.host() << "\n";
+	std::cout << "user_info=" << target_uri.user_info() << "\n";
+	std::cout << "uri=" << target_uri.to_string() << "\n";
+	http_port = target_uri.port();
+	std::cout << "http_port=" << http_port << "\n";
 
-		// check for password in the URI
-		std::string user_info = target_uri.user_info();
-		auto pos = user_info.find(':');
-		if (pos) {
-			// maybe has a password
-			// TODO
-		}
-		else {
-			username = user_info;
-		}
+	std::cout << "is_authority=" << target_uri.is_authority() << "\n";
 
+	// check for password in the URI
+	std::string user_info = target_uri.user_info();
+	auto pos = user_info.find(':');
+	if (pos) {
+		// maybe has a password
+		// TODO
+	}
+	else {
+		username = user_info;
 	}
 
 	// password no longer supported in URI 
@@ -139,33 +194,50 @@ int main(int argc, char* argv[])
 	web::http::http_response data_response; 
 	try {
 		auto response = client.request(methods::GET, builder.to_string());
+		// blocking wait for the http
 		response.wait();
 		data_response = response.get(); 
 	}
 	catch (web::http::http_exception & err) {
 		std::cerr << "Error! " << err.what() << std::endl;
+		std::cerr << "Error! " << err.error_code() << std::endl;
 		return EXIT_FAILURE;
 	}
 
-	web::http::http_headers headers = data_response.headers();
-	std::cout << data_response.to_string() << std::endl;
-	auto json_response = data_response.extract_json();
-	// blocking wait for response
-	// XXX why do I have to .wait() to get data I already have locally?
-	// something to do with the async core of this library?
-	json_response.wait();
-	json::value obj = json_response.get();
-	std::cout << "obj=" << obj << std::endl;
-	json::value success = obj.at(U("success"));
-	json::value data = obj.at(U("data"));
+//	web::http::http_headers headers = data_response.headers();
+//	std::cout << data_response.to_string() << std::endl;
 
+	// extract_json() returns a pplx::task<json::value>
+	auto json_response = data_response.extract_json();
+	// blocking wait for task to finish
+	json_response.wait();
+	// return the contents of the task (the template thingy)
+	json::value value = json_response.get();
+	std::cout << "value=" << value << std::endl;
+
+	// pull out the contents the router sends back
+	json::value success = value.at(U("success"));
+	json::value data = value.at(U("data"));
+
+	// so are we connected or what?
+	std::string connection_state = data.as_string();
+	std::cout << "router is " << connection_state << "\n";
+
+	// start a new get
 	builder.clear();
 	builder.set_path(U("/api/status/wan/devices"));
-	std::cout << builder.to_string() << std::endl;
+//	std::cout << builder.to_string() << std::endl;
 
-	auto response = client.request(methods::GET, builder.to_string());
-
-	response.wait();
+	try {
+		auto response = client.request(methods::GET, builder.to_string());
+		response.wait();
+		data_response = response.get(); 
+	}
+	catch (web::http::http_exception & err) {
+		std::cerr << "Error! " << err.what() << std::endl;
+		std::cerr << "Error! " << err.error_code() << std::endl;
+		return EXIT_FAILURE;
+	}
 //	// try polling out of curiousity
 //	while( ! response.is_done()) {
 //		std::cout << response.is_done() << std::endl;
@@ -173,37 +245,32 @@ int main(int argc, char* argv[])
 //		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 //	}
 
-	data_response = response.get(); 
-	headers = data_response.headers();
-
+//	headers = data_response.headers();
 //	std::cout << data_response.to_string() << std::endl;
 
+	// another task to extract the json
 	json_response = data_response.extract_json();
 	json_response.wait();
-	obj = json_response.get();
-//	std::cout << "obj=" << obj << std::endl;
-
-//	json::value obj = json::value::parse(data_response.to_string());
-//	utility::stringstream_t ss1;
-//	ss1 << data_response.to_string();
-//	json::value obj = json::value::parse(ss1);
-//	json::value obj = json::value::parse(U("{\"success\": true, \"data\": \"connected\"}"));
-	success = obj.at(U("success"));
+	value = json_response.get();
+	success = value.at(U("success"));
 	std::cout << "success=" << success << std::endl;
-	data = obj.at(U("data"));
+	data = value.at(U("data"));
 //	std::cout << "data=" << data << " is array=" << data.is_array() << std::endl;
 
+	// for use with std::optional<json::value>.value_or()
+	json::value no_value(U("n/a"));
+
 	// parse the response
-	json::object obj2 = data.as_object();
-	for (auto ptr = obj2.begin() ; ptr < obj2.end() ; ptr++) {
+	json::object status_obj = data.as_object();
+	for (auto ptr = status_obj.begin() ; ptr != status_obj.end() ; ptr++) {
 		utility::string_t key = ptr->first;
-		json::value value = ptr->second;
+		value = ptr->second;
 		std::cout << "key=" << key << std::endl;
 		json::value config = value.at(U("config"));
 		json::value status = value.at(U("status"));
 		json::value info = value.at(U("info"));
 		json::array connectors = value.at(U("connectors")).as_array();
-		for (auto conn = connectors.begin() ; conn < connectors.end() ; conn++) {
+		for (auto conn = connectors.begin() ; conn != connectors.end() ; conn++) {
 			std::cout << "conn=" << *conn << std::endl;
 		}
 
@@ -218,23 +285,88 @@ int main(int argc, char* argv[])
 		json::value type_ = info.at(U("type"));
 		json::value plugged = status.at(U("plugged"));
 
-		json::value no_value(U("n/a"));
-
-		std::cout << rssi.value_or(no_value) << "\n";
+//		std::cout << rssi.value_or(no_value) << "\n";
 //		std::cout << "type=" << type_ << " plugged=" << plugged << "\n";
 		std::cout << "type=" << type_ << " plugged=" << plugged << " rssi=" << rssi.value_or(no_value) << "\n";
 	}
 
-//	json::array data_array = data.as_array();
+	std::cout << "router is " << connection_state << "\n";
+	boost::format formatter("%|30s| %|10s|    %|-10s| %|-10s|    %|-24s|\n");
+	std::cout << "                          NAME       TYPE    PLUGGED    REASON        SUMMARY\n";
+	for (auto &ptr : status_obj ) {
+		utility::string_t key = ptr.first;
+		value = ptr.second;
 
-//	json::object::object data2 = obj.at(U("data"));
+		json::value config = value.at(U("config"));
+		json::value status = value.at(U("status"));
+		json::value info = value.at(U("info"));
 
-//	for (auto ptr = data_array.begin() ; ptr < data_array.end() ; ptr++) {
-////		std::cout << "ptr=" << *ptr << std::endl;
-//		bool connected = ptr->at(U("connected")).as_bool();
-//		
-//	}
+		// 'at' will throw if key doesn't exist
+		std::string type_ = info.at(U("type")).as_string();
+		bool  plugged = status.at(U("plugged")).as_bool();
+		std::string reason = status.at(U("reason")).as_string();
+		std::string summary = status.at(U("summary")).as_string();
+		std::cout << formatter % key % type_ % (plugged?"true":"false") % reason % summary;
+//		std::cout << boost::format("%|30s| %|10s|    %|-10s| %|-10s|    %|-24s|\n") % key % type_ % (plugged?"true":"false") % reason % summary;
+	}
 
+	for (auto &ptr : status_obj ) {
+		utility::string_t key = ptr.first;
+		value = ptr.second;
+
+//		json::value config = value.at(U("config"));
+//		json::value status = value.at(U("status"));
+//		json::value info = value.at(U("info"));
+
+		json::array connectors = value.at(U("connectors")).as_array();
+		std::cout << "\nconnectors for " << key << "\n";
+		std::cout << "                          NAME  STATE         EXCEPTION TIMEOUT\n";
+		for (auto& conn : connectors ) {
+			// name state exception timeout 
+			json::object conn_obj = conn.as_object();
+
+			for (auto& fields : conn_obj) {
+				utility::string_t key = fields.first;
+				value = fields.second;
+//				std::cout << key << "=" << value << "\n";
+			}
+
+			// can I do something like python's string.join with an iterators cross the connectors' keys?
+			// starting simple, make an array, then directly join
+			std::vector<std::string> connector_keys;
+			for (auto& fields : conn_obj) {
+				utility::string_t key = fields.first;
+				value = fields.second;
+				connector_keys.push_back(key);
+			}
+			std::string all_keys = boost::algorithm::join(connector_keys, ",");
+//			std::cout << "all_keys=" << all_keys << "\n";
+
+//			std::string all_keys2 = boost::algorithm::join(conn_obj.begin(), ",");
+//			std::cout << "all_keys=" << all_keys2 << "\n";
+
+			std::string name = conn_obj.at(U("name")).as_string();
+			json::value state = conn_obj.at(U("state"));
+			json::value exception = conn_obj.at(U("exception"));
+			// timeout may or may not exist
+			std::optional<json::value> timeout;
+			try {
+				timeout = conn_obj.at(U("timeout"));
+			}
+			catch ( json::json_exception& err ) {
+				// pass
+			}
+
+
+//			std::cout << "name=" << name << "\n";
+//			std::cout << "state=" << state << "\n";
+//			std::cout << "exception=" << exception << "\n";
+//			std::cout << timeout << "\n";
+			std::cout << boost::format("%|30s| %|10s|    %|-10s| %|-10s|\n") % name % state % exception % timeout.value_or(no_value);
+		}
+	}
+
+	// tinkering with boost assert
 	BOOST_ASSERT_MSG(true,"hello, world");
 
 	return 0;
