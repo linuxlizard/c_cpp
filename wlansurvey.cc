bf41f2a034a74e890716fabdad73b68e0ab76e0a
David Poole <dpoole@cradlepoint.com>
2018-12-30 13:52:21 -0700

    start of working PUT

--- wlansurvey.cc
+++ wlansurvey.cc
@@ -9,6 +9,11 @@ using namespace web;                        // Common features like URIs.
 using namespace web::http;                  // Common HTTP functionality
 using namespace web::http::client;          // HTTP client features
 
+// TODO get username+password elsewhere
+std::string username { U("admin") };
+std::string password { U("00000000") };
+//std::string password { U("12345") };
+
 static std::optional<json::value> get(http_client& client, uri_builder& builder)
 {
 	web::http::http_response data_response; 
@@ -35,7 +40,7 @@ static std::optional<json::value> get(http_client& client, uri_builder& builder)
 	return value;
 }
 
-static void object_introspect(json::object& jobj)
+void object_introspect(json::object& jobj)
 {
 	// dump the key+value pairs in a json::object
 	for (auto fields: jobj) {
@@ -45,18 +50,22 @@ static void object_introspect(json::object& jobj)
 	}
 }
 
-static bool wlansurvey(std::string& target)
+int channel_to_frequency(int f)
 {
-	if (!web::uri::validate(target)) {
-		std::cerr << "invalid target\n";
-		return false;
-	}
+//	# https://en.wikipedia.org/wiki/List_of_WLAN_channels
+	if (f >= 36)
+		return f*5 + 5000;
+	else if (f < 14)
+		return ((f-1)*5) +2412;
+	else if (f == 14)
+		return 2484;
+	else
+		return 0;
+}
 
+bool wlansurvey(std::string& target)
+{
 	web::uri target_uri = web::uri{target};
-
-	// TODO get username+password elsewhere
-	std::string username { U("admin") };
-	std::string password { U("12345") };
 
 	credentials cred{username, password};
 	http_client_config config;
@@ -130,15 +139,71 @@ static bool wlansurvey(std::string& target)
 			utility::string_t bssid = network.at(U("bssid")).as_string();
 			utility::string_t ssid = network.at(U("ssid")).as_string();
 			int channel = network.at(U("channel")).as_integer();
-//			int frequency = network.at(U("frequency")).as_integer();
+			int frequency = channel_to_frequency(channel);
 			int rssi = network.at(U("rssi")).as_integer();
 			utility::string_t seen = network.at(U("seen")).as_string();
 
-			std::cout << boost::format("%|30s|    %|10s| %|5s| %|5s|    %|-20s|\n") % ssid % bssid % rssi % channel % seen;
+			std::cout << boost::format("%|30s|    %|10s| %|5s| %|5s| %|5s|   %|-20s|\n") % ssid % bssid % rssi % channel % frequency % seen;
 		}
 	}
 
 	return true;
+}
+
+void trigger_scan(std::string& target)
+{
+	std::cout << target << "\n";
+
+	// let's learn how to PUT 
+	std::string path {"/api/control/wlan/radio/0"};
+
+	http::uri uri = http::uri_builder(target).append_path(path).to_uri();
+//	http::uri uri = http::uri(target);
+
+	credentials cred{username, password};
+	http_client_config config;
+	config.set_credentials(cred);
+	http_client client(target, config);
+
+	// maketh the json (stupid to go through all this for something as simple
+	// as {"survey":True} but I'm learning how to use the library 
+//	json::value value {true};
+//	json::object obj;
+//	obj["survey"] = value;
+
+	utility::string_t k {"survey"};
+	json::value v {true};
+
+	// this seems awfully complicated
+	std::vector fields {std::make_pair(k,v)};
+	json::value value =  json::value::object(fields);
+	object_introspect(value.as_object());
+	utility::string_t s = value.serialize();
+	std::cout << s << "\n";
+
+	// this might be simpler
+	value = json::value::parse("{\"survey\":true}");
+	object_introspect(value.as_object());
+	std::cout << value.serialize() << "\n";
+
+	// our firmware wants payload URL encoded 
+	// my python also sends Content-Type: application/x-www-form-urlencoded
+	//
+	// (using BlackJackClient.cpp as example on building this string)
+	utility::ostringstream_t buf;
+	buf << "data=" << value.serialize() ;
+//	buf << "data={\"survey\":true}" ;
+//	buf << "data=%7B%22survey%22%3A+true%7D";
+
+	http_request request(methods::PUT);
+	request.set_request_uri(uri);
+	request.set_body(buf.str());
+	request.headers()["Content-Type"] = "application/x-www-form-urlencoded";
+	std::cout << "request=" << request.to_string() << "\n";
+	http_response response = client.request(request).get();
+//	http_response response = client.request(methods::PUT, path, buf.str()).get();
+	json::value response_json = response.extract_json().get();
+	std::cout << "value=" << response_json << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -150,7 +215,13 @@ int main(int argc, char* argv[])
 	std::string target{argv[1]};
 	std::cout << "target=" << target << "\n";
 
-	wlansurvey(target);
+	if (!web::uri::validate(target)) {
+		std::cerr << "invalid target\n";
+		return EXIT_FAILURE;
+	}
+
+//	wlansurvey(target);
+	trigger_scan(target);
 
 	return EXIT_SUCCESS;
 }
