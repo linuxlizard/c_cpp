8050959dbe9c37565cadce601ee2d646d928ce16
David Poole <dpoole@cradlepoint.com>
2019-04-09 06:52:12 -0600

    small clean-up

--- find-dup-names.cc
+++ find-dup-names.cc
@@ -11,6 +11,7 @@
 namespace fs = std::filesystem;
 
 namespace {
+	// https://en.cppreference.com/w/cpp/regex/basic_regex
 	std::regex src_file { "[a-zA-Z_]+\\.c" };
 }
 
@@ -20,28 +21,30 @@ int main()
 	std::map<std::string, std::string>::iterator iter;
 	bool flag;
 
-	std::string filename, filepath;
-
 	for (auto& f : fs::recursive_directory_iterator(".")) {
-		if (f.is_regular_file()) {
-			std::cout << f.path() << "\n";
-			for (auto& p : f.path()) {
-				std::cout << "p=" << p << "\n";
-			}
+		if (! f.is_regular_file()) {
+			continue;
+		}
+		fs::path path = f.path();
 
-			filename = f.path().filename();
-			std::cout << filename << "\n";
+		std::cout << path << "\n";
+		// iterate over the components in the path
+		for (auto& p : path) {
+			std::cout << "p=" << p << "\n";
+		}
 
-			if (!std::regex_match(filename, src_file)) {
-				continue;
-			}
-			std::cout << "regex match\n";
+		std::string filename = path.filename();
+		std::cout << filename << "\n";
 
-			std::tie(iter, flag) = pathmap.emplace(filename, f.path());
-			if (!flag) {
-				std::cerr << "duplicate filename found " << filename << " at " << iter->second << "\n";
-				throw filename;
-			}
+		if (!std::regex_match(filename, src_file)) {
+			continue;
+		}
+		std::cout << "regex match\n";
+
+		std::tie(iter, flag) = pathmap.emplace(filename, path);
+		if (!flag) {
+			std::cerr << "duplicate filename found " << filename << " at " << iter->second << "\n";
+			throw filename;
 		}
 	}
 
