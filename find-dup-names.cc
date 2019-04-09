e1d14317fd039c71fe6b99fa3f554d4fb9bcdc20
David Poole <dpoole@cradlepoint.com>
2019-04-09 06:41:45 -0600

    tinkering with std::filesystem to find dup filenames
    use std::filesystem to walk a source tree to find duplicate src
    filenames

---
+++ find-dup-names.cc
@@ -0,0 +1,50 @@
+// davep 20190408 walk a filesystem, find duplicate filenames
+// Learning exercise for std::filesystem std::regex std::map
+//
+// gcc -g -Wall -o find-dup-names find-dup-names.cc -std=c++17 -lstdc++ -lstdc++fs
+//
+#include <iostream>
+#include <filesystem>
+#include <map>
+#include <regex>
+
+namespace fs = std::filesystem;
+
+namespace {
+	std::regex src_file { "[a-zA-Z_]+\\.c" };
+}
+
+int main()
+{
+	std::map<std::string, std::string> pathmap;
+	std::map<std::string, std::string>::iterator iter;
+	bool flag;
+
+	std::string filename, filepath;
+
+	for (auto& f : fs::recursive_directory_iterator(".")) {
+		if (f.is_regular_file()) {
+			std::cout << f.path() << "\n";
+			for (auto& p : f.path()) {
+				std::cout << "p=" << p << "\n";
+			}
+
+			filename = f.path().filename();
+			std::cout << filename << "\n";
+
+			if (!std::regex_match(filename, src_file)) {
+				continue;
+			}
+			std::cout << "regex match\n";
+
+			std::tie(iter, flag) = pathmap.emplace(filename, f.path());
+			if (!flag) {
+				std::cerr << "duplicate filename found " << filename << " at " << iter->second << "\n";
+				throw filename;
+			}
+		}
+	}
+
+	return 0;
+}
+
