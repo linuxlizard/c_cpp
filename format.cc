d2a423061d7443472ce5dadc50b8c469e5642999
David Poole <dpoole@cradlepoint.com>
2021-07-30 20:05:24 -0600

    convert to fmt

--- format.cc
+++ format.cc
@@ -1,11 +1,10 @@
-/* davep 20181227 ; tinkering with boost::format */
 #include <iostream>
-#include <boost/format.hpp>
 #include <cstdlib>
 
 /* davep 20190328 ; tinkering with fmt */
+/* davep 20210716 ; upgrade completely to fmt (no more boost::format) */
 // http://fmtlib.net/latest/api.html#format
-#include "fmt/format.h"
+#include <fmt/format.h>
 
 typedef std::tuple<std::string, std::string, bool, std::string, std::string> status;
 
@@ -22,31 +21,52 @@ std::array<status,4> status_list {
 
 // http://fmtlib.net/latest/api.html#format
 // "Formatting user-defined types"
-namespace fmt {
-	template <>
-	struct formatter<status> {
-	  template <typename ParseContext>
-	  constexpr auto parse(ParseContext &ctx) { return ctx.begin(); }
+template <> 
+struct fmt::formatter<status> 
+{
+	char presentation = 'S';
 
-	  template <typename FormatContext>
-	  auto format(const status &p, FormatContext &ctx) {
-		std::string key;
-		std::string type_;
-		bool plugged;
-		std::string reason;
-		std::string summary;
-		std::tie(key, type_, plugged, reason, summary) = p;
+	std::string status_fmt { "{0:30s} {1:10s}    {2:10s} {3:10s}    {4:24s}" };
 
-		// didn't know get<type> was a possibility, too
-		plugged = std::get<bool>(p);
-//		return format_to(ctx.begin(), "{0:>40s} {1:<10s} {2:<7} {3:<10s} {4}", 
-//				key, type_, plugged, reason, summary);
+	// Parses format specifications of the form ['f' | 'e'].
+	constexpr auto parse(format_parse_context& ctx) -> decltype(ctx.begin()) {
+		// [ctx.begin(), ctx.end()) is a character range that contains a part of
+		// the format string starting from the format specifications to be parsed,
+		// e.g. in
+		//
+		//   fmt::format("{:f} - point of interest", point{1, 2});
+		//
+		// the range will contain "f} - point of interest". The formatter should
+		// parse specifiers until '}' or the end of the range. In this example
+		// the formatter should parse the 'f' specifier and return an iterator
+		// pointing to '}'.
 
-		return format_to(ctx.begin(), "{0:>40s} {1:<10s} {2:<7} {3:<10s} {4}", 
-				std::get<0>(p), std::get<1>(p), std::get<2>(p), std::get<3>(p), std::get<4>(p));
-	  }
-	};
-}
+		// Parse the presentation format and store it in the formatter:
+		auto it = ctx.begin(), end = ctx.end();
+		if (it != end && (*it == 'S')) presentation = *it++;
+//		if (it != end && (*it == 'f' || *it == 'e')) presentation = *it++;
+
+		// Check if reached the end of the range:
+		if (it != end && *it != '}')
+			throw fmt::format_error("invalid format");
+
+		// Return an iterator past the end of the parsed range:
+		return it;
+	}
+
+
+	// Formats the point p using the parsed format specification (presentation)
+	// stored in this formatter.
+	template <typename FormatContext>
+	auto format(const status& p, FormatContext& ctx) -> decltype(ctx.out()) {
+		// ctx.out() is an output iterator to write to.
+		auto [ key, type_, plugged, reason, summary ] = p;
+		return fmt::format_to(
+				ctx.out(),
+				status_fmt, key, type_, (plugged?"true":"false"), reason, summary
+				);
+	}
+};
 
 int main(void)
 {
@@ -56,16 +76,14 @@ int main(void)
 	std::string reason;
 	std::string summary;
 
-	std::cout << "                          NAME       TYPE    PLUGGED    REASON        SUMMARY\n";
-	for (auto iter=status_list.begin() ; iter != status_list.end() ; ++iter ) {
-		std::tie(key, type_, plugged, reason, summary) = *iter;
-//		std::cout << boost::format("%1% %2% %3% %4% %5%\n") % key % type_ % plugged % reason % summary;
-//		std::cout << boost::format("%|s| %|s| %|s| %|s| %|s|\n") % key % type_ % plugged % reason % summary;
-//		std::cout << boost::format("%|20s| %|s| %|s| %|s| %|s|\n") % key % type_ % plugged % reason % summary;
-		std::cout << boost::format("%|30s| %|10s|    %|-10s| %|-10s|    %|-24s|\n") % key % type_ % (plugged?"true":"false") % reason % summary;
-	}
+	std::string status_fmt { "{0:30s} {1:10s}    {2:10s} {3:10s}    {4:24s}\n" };
+	fmt::print( status_fmt, "NAME", "TYPE", "PLUGGED", "REASON", "SUMMARY" );
 
-//	std::cout << boost::format("%25s %1% %2% %3% %4%\n") % key % type_ % plugged % reason % summary;
+	for (auto iter=status_list.begin() ; iter != status_list.end() ; ++iter ) {
+		auto [ key, type_, plugged, reason, summary ] = *iter;
+
+		fmt::print(status_fmt, key, type_, (plugged?"true":"false"), reason, summary);
+	}
 
 	/* davep 20190328 ; fiddling with fmt (http://fmtlib.net) */
 	// http://fmtlib.net/latest/api.html#format
@@ -75,16 +93,14 @@ int main(void)
 	std::cout << message << "\n";
 	fmt::print("Elapsed time: {0:.2f} seconds\n", 1.23);
 	fmt::print("Elapsed time: {s:.2f} seconds\n", fmt::arg("s", 1.23));
-	std::string smsg = fmt::format("{}\n", status_list.at(0));
 
-	std::cout << "                                    NAME TYPE       PLUGGED REASON     SUMMARY\n";
+	fmt::print( status_fmt, "NAME", "TYPE", "PLUGGED", "REASON", "SUMMARY" );
 	for (auto &status : status_list) {
-//	for (auto iter=status_list.begin() ; iter != status_list.end() ; ++iter ) {
-		std::string smsg = fmt::format("{}\n", status);
-//		std::string smsg = fmt::format("{}\n", *iter);
+		std::string smsg = fmt::format("{:S}\n", status);
 		std::cout << smsg;
 	}
 
+	// utf8?
 	std::string flip = fmt::format("flip {}", u8"(╯°□°）╯︵ ┻━┻");
 	std::cout << flip << "\n";
 
